0b865915d87ca3ae08aa92d735ffc84d5ede2d0f
Mandar Nalhe <152013939+MandarNalhe@users.noreply.github.com>
2025-05-10 02:07:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,8 @@ app.use('/api/cases', caseRoutes);
 
 // Connect to MongoDB
 console.log("Connecting to:", process.env.MONGO_URI); 
+console.log("Connecting to:", process.env.PORT);
+
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
