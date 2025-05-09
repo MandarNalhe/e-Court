1319a36a5daa59a791f7f360233e4a063f3aa864
Mandar Nalhe <152013939+MandarNalhe@users.noreply.github.com>
2025-05-10 02:37:07 +0530

    Update Register.jsx

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -44,7 +44,7 @@ const register = ()=>{
       const handleLoginSubmit = async (e) => {
         e.preventDefault();
         try {
-          const response = await fetch("http://localhost:5000/api/auth/login", {
+          const response = await fetch("https://backend-production-969c.up.railway.app/api/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(loginData),
@@ -88,7 +88,7 @@ const register = ()=>{
         e.preventDefault(); 
     
         try {
-          const response = await fetch("http://localhost:5000/api/auth/register", {
+          const response = await fetch("https://backend-production-969c.up.railway.app/api/auth/register", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
