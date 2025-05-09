d30ad6d743d6beb13394ec3b7feb96de6ee0cba7
Mandar Nalhe <152013939+MandarNalhe@users.noreply.github.com>
2025-05-10 04:22:26 +0530

    Update VideoCall.jsx

--- frontend/src/Pages/VideoCall.jsx
+++ frontend/src/Pages/VideoCall.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://backend-production-969c.up.railway.app");
 
 const VideoCallPage = () => {
   const localVideoRef = useRef(null);
